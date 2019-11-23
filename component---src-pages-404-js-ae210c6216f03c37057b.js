4e0742d0c35e07b9d7170fd712bdef1a2bf71231
Arpan Sahoo <arpansahooedu@gmail.com>
2019-11-23 13:21:49 -0500

    Updates

---
+++ component---src-pages-404-js-ae210c6216f03c37057b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),c=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("div",{class:"not-found"},l.a.createElement("h1",null,"Page currently under development."),l.a.createElement("p",null,"Don't be too sad. Check back later!")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ae210c6216f03c37057b.js.map
---
+++ component---src-pages-our-team-js-fbdb4540eb6aebb0fe60.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{uPNo:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"Our Team"}),l.a.createElement("h1",null,"Our team"),l.a.createElement("p",null,"in development"))}}}]);
+//# sourceMappingURL=component---src-pages-our-team-js-fbdb4540eb6aebb0fe60.js.map
