ae4885a112dc7ebc7655f9d2e79132d93906d22a
Arpan Sahoo <arpansahooedu@gmail.com>
2019-11-23 09:25:30 -0500

    Updates

---
+++ component---src-pages-page-2-js-0df15caf49690e527996.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),c=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"Our Team"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-0df15caf49690e527996.js.map
